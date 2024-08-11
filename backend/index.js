9325d12a4d5b6d2117114f3ef1100d65bf929895
Animesh  Rathore <animeshrathore255@gmail.com>
2024-08-11 17:25:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,9 +13,15 @@ dotenv.config();
 
 connectDB();
 
-app.use(function (request, response, next) {
-  response.header("Access-Control-Allow-Origin", "*");
-  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 
+  'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if( req.method === 'OPTIONS'){
+    req.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
   next();
 });
 
