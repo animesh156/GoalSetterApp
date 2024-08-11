bbd148a76a08c1ad8bff101ace0566af29a12efe
Animesh  Rathore <animeshrathore255@gmail.com>
2024-08-11 17:40:02 +0530

    Update authService.js

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-const registerRoute = "https://goal-setter-backend-vert.vercel.app/api/users/register"
+const registerRoute = "https://goal-setter-backend-chi.vercel.app/api/users/register"
 
-const loginRoute =    "https://goal-setter-backend-vert.vercel.app/api/users/login"
+const loginRoute =    "https://goal-setter-backend-chi.vercel.app/api/users/login"
 
 
 // Register user
