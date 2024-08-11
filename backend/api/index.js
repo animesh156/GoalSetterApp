bc2a932409e6ff8be140e05fc6865931213103ad
animesh <animeshrathore255@gmail.com>
2024-08-11 16:08:10 +0530

    updated vercel

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv");
 const port = process.env.PORT || 5000;
-const goalRoutes = require("./routes/goalRoutes");
-const userRoutes = require("./routes/userRoutes");
-const { errorHandler } = require("./middleware/errorMiddleware");
-const connectDB = require("./config/db");
+const goalRoutes = require("../routes/goalRoutes");
+const userRoutes = require("../routes/userRoutes");
+const { errorHandler } = require("../middleware/errorMiddleware");
+const connectDB = require("../config/db");
 var cors = require('cors')
 
 dotenv.config();
